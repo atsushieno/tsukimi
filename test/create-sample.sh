982351f340fa0d4791c596d6b0a45c9758f87763
atsushieno <atsushieno@e286893d-4655-0410-8db1-c147b91c484a>
2009-02-11 17:12:13 +0000

    remove extra files.
    git-svn-id: https://tsukimi.googlecode.com/svn/trunk@143 e286893d-4655-0410-8db1-c147b91c484a

--- test/create-sample.sh
+++
@@ -1,6 +0,0 @@
-mkdir $1
-cd $1
-cp ../../Processing.Core.dll* .
-sed -e "s/REPLACE_HERE/FooBar/g" <../AppManifest_Template.xaml > AppManifest.xaml
-mono --debug ../../processingimporter.exe ../$2 > $1.cs
-mxap
