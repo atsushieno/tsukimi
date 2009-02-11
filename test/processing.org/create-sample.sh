5a2d72f2f5ec69ecc74aad2bd9ae914a964ccf88
atsushieno <atsushieno@e286893d-4655-0410-8db1-c147b91c484a>
2009-02-11 17:09:59 +0000

    fix directories.
    git-svn-id: https://tsukimi.googlecode.com/svn/trunk@142 e286893d-4655-0410-8db1-c147b91c484a

--- test/processing.org/create-sample.sh
+++ test/processing.org/create-sample.sh
@@ -1,6 +1,6 @@
 mkdir $1
 cd $1
-cp ../../Processing.Core.dll* .
+cp ../../../Processing.Core.dll* .
 sed -e "s/REPLACE_HERE/FooBar/g" <../AppManifest_Template.xaml > AppManifest.xaml
-mono --debug ../../processingimporter.exe ../$2 > $1.cs
+mono --debug ../../../processingimporter.exe ../$2 > $1.cs
 mxap
