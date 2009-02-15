ddba1c34b99630f0186411901f165cabdf3141eb
Atsushi Eno <atsushi@linux-avjl.(none)>
2009-02-15 18:31:07 +0900

    Improved xap importer class and delegated most of the driver code into ProcessingXapImporterClr, while leaving CoreClr importer possible.

--- ProcessingXapImporter.cs
+++ ProcessingXapImporter.cs
@@ -1,28 +1,168 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Net;
+using System.Reflection;
 using System.Threading;
 using ProcessingCli.Parser;
 
 namespace ProcessingCli
 {
-	public class ProcessingXapImporter
+	public class ProcessingXapImporterClr : ProcessingXapImporter
 	{
-		public static void ImportFile (ProcessingProjectSource source)
+		bool created;
+		DirectoryInfo dir;
+
+		public ProcessingXapImporterClr (ProcessingProjectSource source, bool useCurrent)
+			: base (source)
 		{
+			string path = useCurrent ?
+				Directory.GetCurrentDirectory () :
+				Path.Combine (Path.GetTempPath (), "tsukimi-" + Guid.NewGuid ());
+			if (!Directory.Exists (path)) {
+				dir = Directory.CreateDirectory (path);
+				created = true;
+			}
+			else
+				dir = new DirectoryInfo (path);
+		}
+
+		public override Stream CreateOutputStream (string name)
+		{
+			return File.Create (Path.Combine (dir.FullName, name));
+		}
+
+		public override void CopyDependentAssemblies ()
+		{
+				string exedir = Path.GetDirectoryName (new Uri (Assembly.GetEntryAssembly ().CodeBase).LocalPath);
+				string dll = "Processing.Core.dll";
+				string mdb = dll + ".mdb";
+				Copy (exedir, dir.FullName, dll);
+				Copy (exedir, dir.FullName, mdb);
+		}
+
+		public override void CreateApplicationPackage ()
+		{
+			ProcessingProjectSource p = Source;
+
+			ProcessStartInfo psi = new ProcessStartInfo ();
+			psi.FileName = "mxap";
+			psi.Arguments = "--application-name " + p.NamespaceName;
+			psi.WorkingDirectory = dir.FullName;
+			Process proc = Process.Start (psi);
+			proc.WaitForExit ();
+			if (proc.ExitCode != 0) {
+				Console.Error.WriteLine ("mxap failed with status code {0}", proc.ExitCode);
+				ErrorCode = -1;
+				return;
+			}
+			if (p.DataFiles.Count > 0) {
+				var l = new List<string> ();
+				l.Add ("-j");
+				l.Add (p.NamespaceName + ".xap");
+				foreach (var uri in p.DataFiles)
+					l.Add (uri.LocalPath);
+				psi = new ProcessStartInfo ();
+				psi.FileName = "zip";
+				psi.WorkingDirectory = dir.FullName;
+				psi.Arguments = String.Join (" ", l.ToArray ());
+				proc = Process.Start (psi);
+				proc.WaitForExit ();
+				if (proc.ExitCode != 0) {
+					Console.Error.WriteLine ("zip failed with status code {0}", proc.ExitCode);
+					ErrorCode = -2;
+					return;
+				}
+			}
+		}
+
+		public override void Dispose ()
+		{
+			if (created)
+				dir.Delete ();
+		}
+
+		static void Copy (string exedir, string curdir, string file)
+		{
+			string srcfile = Path.Combine (exedir, file);
+			string dstfile = Path.Combine (curdir, file);
+			if (File.Exists (srcfile)) {
+				if (File.Exists (dstfile))
+					File.Delete (dstfile);
+				File.Copy (srcfile, dstfile);
+			}
+		}
+	}
+
+	public class ProcessingXapImporterCoreClr : ProcessingXapImporter
+	{
+		public ProcessingXapImporterCoreClr (ProcessingProjectSource source)
+			: base (source)
+		{
+			isf = IsolatedStorageFile.GetUserStoreForApplication ();
+		}
+
+		IsolatedStorageFile isf;
+
+		public override Stream CreateOutputStream (string name)
+		{
+			return new IsolatedStorageFileStream (name, FileMode.Create, isf);
+		}
+
+		public override void CopyDependentAssemblies ()
+		{
+#if NET_2_1
+			var ass = "Processing.Core.dll";
+			using (var a = Deployment.Current.AssemblyParts [ass]) {
+				using (var f = CreateOutputStream (ass)) {
+					byte [] arr = new byte[a.Length];
+					a.Read (arr, 0, arr.Length);
+					f.Write (arr, 0, arr.Length);
+				}
+			}
+#endif
+		}
+
+		public override void CreateApplicationPackage ()
+		{
+			// It is not possible to run "mxap" on moonlight (or siverlight either).
+			throw new NotImplementedException ("We need to sort out how to compile the output .cs files");
+		}
+
+		public override void Dispose ()
+		{
+			isf.Close ();
+		}
+	}
+
+	public abstract class ProcessingXapImporter : IDisposable
+	{
+		protected ProcessingXapImporter (ProcessingProjectSource source)
+		{
+			Source = source;
+		}
+
+		public ProcessingProjectSource Source { get; private set; }
+
+		public int ErrorCode { get; set; }
+
+		public abstract void Dispose ();
+		public abstract Stream CreateOutputStream (string name);
+		public abstract void CopyDependentAssemblies ();
+		public abstract void CreateApplicationPackage ();
+
+		public void GenerateFile ()
+		{
+			ProcessingProjectSource source = Source;
 			var sw = new StringWriter ();
 			ProcessingSourceImporter.Import (source, sw);
-			using (var isf = IsolatedStorageFile.GetUserStoreForApplication ()) {
-				using (var writer = new StreamWriter (new IsolatedStorageFileStream (source.NamespaceName + ".cs", FileMode.Create, isf))) {
-					writer.Write (sw.ToString ());
-				}
-				using (var writer = new StreamWriter (new IsolatedStorageFileStream ("AppManifest.xaml", FileMode.Create, isf))) {
-					writer.Write (CreateAppManifest (source.NamespaceName));
-				}
-				// FIXME: run "mxap", but it requires Process support.
-				// It is not possible on moonlight (or siverlight either).
+			using (var writer = new StreamWriter (CreateOutputStream (source.NamespaceName + ".cs"))) {
+				writer.Write (sw.ToString ());
+			}
+			using (var writer = new StreamWriter (CreateOutputStream ("AppManifest.xaml"))) {
+				writer.Write (CreateAppManifest (source.NamespaceName));
 			}
 		}
 
--- driver.cs
+++ driver.cs
@@ -9,21 +9,18 @@ namespace ProcessingCli
 {
 	public class CommandLineConverter
 	{
-		public static void Main (string [] args)
+		public static int Main (string [] args)
 		{
-			new CommandLineConverter ().Run (args);
-		}
-
-		public void Run (string [] args)
-		{
-			string exedir = Path.GetDirectoryName (new Uri (Assembly.GetEntryAssembly ().CodeBase).LocalPath);
-			string curdir = Directory.GetCurrentDirectory ();
 			bool do_package = false;
+			List<string> files = new List<string> ();
 			foreach (string s in args) {
 				if (s == "--xap") {
 					do_package = true;
 					continue;
 				}
+				files.Add (s);
+			}
+			foreach (string s in files) {
 				ProcessingProjectSource p;
 				if (Directory.Exists (s))
 					p = ProcessingProjectSource.FromDirectory (s);
@@ -32,58 +29,23 @@ namespace ProcessingCli
 				Console.WriteLine ("/* ===============");
 				Console.WriteLine (p);
 				Console.WriteLine ("================*/");
-				StringWriter sw = new StringWriter ();
-				ProcessingSourceImporter.Import (p, sw);
 
 				if (!do_package) {
+					StringWriter sw = new StringWriter ();
+					ProcessingSourceImporter.Import (p, sw);
 					Console.WriteLine (sw);
 					continue;
 				}
 
-				using (var tw = File.CreateText (p.NamespaceName + ".cs"))
-					tw.Write (sw.ToString ());
-				using (var tw = File.CreateText ("AppManifest.xaml"))
-					tw.Write (CreateAppManifest (p.NamespaceName));
-				string dll = "Processing.Core.dll";
-				string mdb = dll + ".mdb";
-				Copy (exedir, curdir, dll);
-				Copy (exedir, curdir, mdb);
-				Process.Start ("mxap", "--application-name " + p.NamespaceName).WaitForExit ();
-				if (p.DataFiles.Count > 0) {
-					var l = new List<string> ();
-					l.Add ("-j");
-					l.Add (p.NamespaceName + ".xap");
-					foreach (var uri in p.DataFiles)
-						l.Add (uri.LocalPath);
-					Process.Start ("zip", String.Join (" ", l.ToArray ())).WaitForExit ();
-				}
+				ProcessingXapImporterClr importer = new ProcessingXapImporterClr (p, true);
+				importer.GenerateFile ();
+				importer.CopyDependentAssemblies ();
+				importer.CreateApplicationPackage ();
+				if (importer.ErrorCode != 0)
+					return importer.ErrorCode;
 			}
+			return 0;
 		}
-
-		static void Copy (string exedir, string curdir, string file)
-		{
-			string srcfile = Path.Combine (exedir, file);
-			string dstfile = Path.Combine (curdir, file);
-			if (File.Exists (srcfile)) {
-				if (File.Exists (dstfile))
-					File.Delete (dstfile);
-				File.Copy (srcfile, dstfile);
-			}
-		}
-
-		const string manifest_template = @"
-<Deployment xmlns='http://schemas.microsoft.com/client/2007/deployment' xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml' EntryPointAssembly='REPLACE_HERE' EntryPointType='REPLACE_HERE.App' RuntimeVersion='2.0.31005.0'>
-  <Deployment.Parts>
-    <AssemblyPart x:Name='Proce55ing.Core' Source='Proce55ing.Core.dll' />
-    <AssemblyPart x:Name='REPLACE_HERE' Source='REPLACE_HERE.dll' />
-  </Deployment.Parts>
-</Deployment>";
-
-		public static string CreateAppManifest (string namespaceName)
-		{
-			return manifest_template.Replace ("REPLACE_HERE", namespaceName);
-		}
-
 	}
 }
 
