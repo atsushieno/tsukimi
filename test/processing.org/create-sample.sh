a9732ee57d3c450dede21d502655821daa6d8fbd
Atsushi Eno <atsushi@linux-6a27.site>
2009-05-18 01:28:26 +0900

    Fix tool path.

--- test/processing.org/create-sample.sh
+++ test/processing.org/create-sample.sh
@@ -1,6 +1,6 @@
 mkdir $1
 cd $1
-cp ../../../Processing.Core.dll* .
+cp ../../../build/Processing.Core.dll* .
 sed -e "s/REPLACE_HERE/FooBar/g" <../AppManifest_Template.xaml > AppManifest.xaml
-mono --debug ../../../tsukimi-tool.exe ../$2 > $1.cs
+mono --debug ../../../build/tsukimi-tool.exe ../$2 > $1.cs
 mxap
