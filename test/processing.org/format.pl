aa12d45d91897a01e50bec46b3dcf3f6732025b9
Atsushi Eno <atsushi@linux-6a27.site>
2009-05-18 01:47:58 +0900

    a new test result index page generator.

---
+++ test/processing.org/format.pl
@@ -0,0 +1,28 @@
+#
+# This script could be used once example-batch-importer ran through the
+# entire examples. Usage:
+#	find . -name *.html > index.txt
+#	format.pl < index.txt > index.html
+# Then upload the entire output directory.
+@line = <STDIN>;
+print <<END;
+<html>
+<head>
+<title>tsukimi test: imported processing.org examples</title>
+</head>
+<body>
+<h1>tsukimi test: imported from processing.org examples</h1>
+
+<ul>
+END
+
+foreach $link (@line) {
+	print "<li><a href=\"$link\">$link</a></li>";
+}
+
+print <<END;
+</ul>
+</body>
+</html>
+END
+
