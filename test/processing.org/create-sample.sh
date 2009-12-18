1ddd860fd7ebbbf11998f80b3af1b239d420be47
Atsushi Eno <atsushi@linux-zdkc.site>
2009-12-18 14:42:48 +0900

    Fix path to tsukimi-tool.

--- test/processing.org/create-sample.sh
+++ test/processing.org/create-sample.sh
@@ -2,5 +2,5 @@ mkdir $1
 cd $1
 cp ../../../build/Processing.Core.dll* .
 sed -e "s/REPLACE_HERE/FooBar/g" <../AppManifest_Template.xaml > AppManifest.xaml
-mono --debug ../../../build/tsukimi-tool.exe ../$2 > $1.cs
+tsukimi-tool ../$2 > $1.cs
 mxap
